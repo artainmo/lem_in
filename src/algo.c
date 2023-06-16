e591203d40db6d2dafbabd905cb193036d824c32
artainmo <tainmontarthur@icloud.com>
2023-06-16 16:06:35 +0200

    protect against start and end room not being linked

--- src/algo.c
+++ src/algo.c
@@ -4,18 +4,32 @@
 // {
 // }
 
-static int go_to_end_room(t_room **queue, t_room **visited, char *end_room)
+static int go_to_end_room(t_antFarm *af, t_room *ant_room) //Here we use the "Breadth First Search" algorithm
 {
+  t_room **visited;
+  t_room **queue;
   int i;
 
+  if (!(visited = malloc(sizeof(t_room *) * room_amount(af) + 1)))
+    ft_malloc_error();
+  *visited = NULL;
+  if (!(queue = malloc(sizeof(t_room *) * room_amount(af) + 1)))
+    ft_malloc_error();
+  *queue = NULL;
+  add_room_end_array(visited, ant_room);
+  add_room_end_array(queue, ant_room);
   while (queue[0])
   {
     i = 0;
     while (queue[0]->connections[i])
     {
       view_stacks(queue, visited);
-      if (ft_strcmp(queue[0]->connections[i]->name, end_room))
+      if (ft_strcmp(queue[0]->connections[i]->name, af->end_room->name))
+      {
+        free(visited);
+        free(queue);
         return 1;
+      }
       if (!queue[0]->connections[i]->ants && //If the room is already filled with ant neglect it
             !room_in_array(visited, queue[0]->connections[i])) //If you already visited the room neglect it
       {
@@ -27,31 +41,9 @@ static int go_to_end_room(t_room **queue, t_room **visited, char *end_room)
     }
     remove_room_front_array(queue);
   }
-  return 0;
-}
-
-static void verify_start_room_is_linked_to_end_room(t_antFarm *af)
-{
-  t_room **visited;
-  t_room **queue;
-  t_room *ant_room;
-  int ret;
-
-  ant_room = af->start_room;
-  if (!(visited = malloc(sizeof(t_room *) * room_amount(af) + 1)))
-    ft_malloc_error();
-  *visited = NULL;
-  if (!(queue = malloc(sizeof(t_room *) * room_amount(af) + 1)))
-    ft_malloc_error();
-  *queue = NULL;
-  add_room_end_array(visited, ant_room);
-  add_room_end_array(queue, ant_room);
-  ret = go_to_end_room(queue, visited, af->end_room->name);
   free(visited);
   free(queue);
-  if (!ret)
-    ft_error("lem-in: Error: Start and end room are not linked.\n");
-  exit(0);
+  return 0;
 }
 
 void algo(t_antFarm *af)
@@ -59,7 +51,9 @@ void algo(t_antFarm *af)
   t_room **ant_room;
   t_room *old_room;
 
-  verify_start_room_is_linked_to_end_room(af);
+  if (!go_to_end_room(af, af->start_room))
+    ft_error("lem-in: Error: Start and end room are not linked.\n");
+  exit(0);
   if (!(ant_room = malloc(sizeof(t_room *) * af->ants_amount)))
     ft_malloc_error();
   for (int i = 0; i < af->ants_amount; i++) { ant_room[i] = af->start_room; }
