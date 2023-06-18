2962c0e918368355de770f9c070c558f9bedc482
artainmo <tainmontarthur@icloud.com>
2023-06-18 13:44:18 +0200

    segfault/infinite-loop on BIG tests

--- tests/automated.sh
+++ tests/automated.sh
@@ -14,3 +14,11 @@ Test
 echo '\033[0;36m./generator_osx --flow-thousand\033[0m'
 ./generator_osx --flow-thousand > generated.map
 Test
+
+echo '\033[0;36m./generator_osx --big\033[0m'
+./generator_osx --big > generated.map
+Test
+
+echo '\033[0;36m./generator_osx --big-superposition\033[0m'
+./generator_osx --big-superposition > generated.map
+Test
